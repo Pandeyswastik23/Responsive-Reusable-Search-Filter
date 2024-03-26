ee5a62d6dc20c8dd80375d76f33ab711e06e85f1
Swastik Pandey <swastikpandey@Swastiks-MacBook-Air.local>
2024-03-26 19:04:29 +0530

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App ()
   return (
     <div className="App">
 
-      <NewSearch placeholder = "Enter a country name..." data={Data}/>
+      <NewSearch placeholder = "Enter a country na" data={Data}/>
         {/* <SearchBar placeholder = "Enter Country name..." data = {Data}/> */}
       
        
